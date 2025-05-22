155d1c5be7a532337b71661ec05975eb3d2a773e
GiuseppeMonaco <61635120+GiuseppeMonaco@users.noreply.github.com>
2025-05-22 15:32:49 +0200

    Create TokenManager interface & implementation

---
+++ src/main/java/it/trenical/server/auth/BiMapTokenManager.java
@@ -0,0 +1,40 @@
+package it.trenical.server.auth;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import it.trenical.client.auth.SessionToken;
+import it.trenical.common.User;
+import it.trenical.common.UserData;
+
+public enum BiMapTokenManager implements TokenManager {
+    INSTANCE;
+
+    // Map user's email with current session token
+    private final BiMap<User, SessionToken> activeSessions = HashBiMap.create();
+
+    @Override
+    public synchronized User getUser(SessionToken token) {
+        return activeSessions.inverse().getOrDefault(token, null);
+    }
+
+    @Override
+    public synchronized SessionToken getToken(User user) {
+        user = new UserData(user.getEmail());
+        SessionToken token;
+        if (activeSessions.containsKey(user)) {
+            token = activeSessions.get(user);
+        } else {
+            do {
+                token = SessionToken.newRandomToken();
+            } while (activeSessions.inverse().containsKey(token));
+            activeSessions.put(user, token);
+        }
+        return token;
+    }
+
+    @Override
+    public synchronized boolean remove(SessionToken token) {
+        return activeSessions.inverse().remove(token) != null;
+    }
+
+}
--- src/main/java/it/trenical/server/auth/GrpcAuthImpl.java
+++ src/main/java/it/trenical/server/auth/GrpcAuthImpl.java
@@ -1,10 +1,7 @@
 package it.trenical.server.auth;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
 import io.grpc.stub.StreamObserver;
 import it.trenical.client.auth.SessionToken;
-import it.trenical.common.User;
 import it.trenical.grpc.*;
 import it.trenical.server.db.DatabaseConnection;
 import it.trenical.server.db.SQLite.SQLiteConnection;
@@ -22,8 +19,7 @@ public class GrpcAuthImpl extends AuthServiceGrpc.AuthServiceImplBase {
 
     private final DatabaseConnection db = SQLiteConnection.getInstance();
 
-    // Map user's email with current session token
-    private final BiMap<String, SessionToken> activeSessions = HashBiMap.create();
+    private final TokenManager tokenManager = BiMapTokenManager.INSTANCE;
 
     @Override
     public void login(LoginRequest request, StreamObserver<LoginReply> responseObserver) {
@@ -39,7 +35,7 @@ public class GrpcAuthImpl extends AuthServiceGrpc.AuthServiceImplBase {
 
         SessionToken token;
         if (realUser != null && verifyPassword(user.getPassword(),realUser.getPassword())) {
-            token = getToken(user);
+            token = tokenManager.getToken(user);
             logger.info(String.format("User authenticated successfully: %s", user.getEmail()));
         } else {
             token = SessionToken.newInvalidToken();
@@ -56,13 +52,8 @@ public class GrpcAuthImpl extends AuthServiceGrpc.AuthServiceImplBase {
     @Override
     public void logout(LogoutRequest request, StreamObserver<LogoutReply> responseObserver) {
 
-        SessionToken token = new SessionToken(request.getToken());
-        boolean done = false;
-        if (activeSessions.inverse().containsKey(token)) {
-            logger.info(String.format("Deauthenticated user: %s", activeSessions.inverse().get(token)));
-            activeSessions.inverse().remove(token);
-            done = true;
-        }
+        boolean done = tokenManager.remove(new SessionToken(request.getToken()));
+        if (done) logger.info(String.format("Deauthenticated user: %s", request.getToken()));
 
         LogoutReply reply = LogoutReply.newBuilder().setIsDone(done).build();
         responseObserver.onNext(reply);
@@ -88,27 +79,13 @@ public class GrpcAuthImpl extends AuthServiceGrpc.AuthServiceImplBase {
         if (exists) {
             token = SessionToken.newInvalidToken();
         } else {
-            token = getToken(user);
+            token = tokenManager.getToken(user);
             logger.info(String.format("User signed up successfully: %s", user.getEmail()));
         }
 
         SignupReply reply = SignupReply.newBuilder().setToken(token.token()).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
-    }
-
-    private SessionToken getToken(User user) {
-        SessionToken token;
-        if (activeSessions.containsKey(user.getEmail())) {
-            token = activeSessions.get(user.getEmail());
-            logger.info(String.format("User has already a token: %s", user.getEmail()));
-        } else {
-            do {
-                token = SessionToken.newRandomToken();
-            } while (activeSessions.inverse().containsKey(token));
-            activeSessions.put(user.getEmail(), token);
-        }
-        return token;
     }
 
 }
