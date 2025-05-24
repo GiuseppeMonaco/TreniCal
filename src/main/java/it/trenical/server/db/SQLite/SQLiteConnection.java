98216334ad1bb8ae51946de4c16dfaa038998464
GiuseppeMonaco <61635120+GiuseppeMonaco@users.noreply.github.com>
2025-05-24 15:29:03 +0200

    Add multiple DB support to SQLiteConnection

--- src/main/java/it/trenical/server/db/SQLite/SQLiteConnection.java
+++ src/main/java/it/trenical/server/db/SQLite/SQLiteConnection.java
@@ -2,24 +2,32 @@ package it.trenical.server.db.SQLite;
 
 import it.trenical.server.db.DatabaseConnection;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class SQLiteConnection implements DatabaseConnection {
 
-    // Singleton class
-    private static SQLiteConnection instance;
+    /**
+     * "Singleton" classes
+     * There will be one instance for every path
+     */
+    private static final Map<Path,SQLiteConnection> instances = new HashMap<>();
 
     private static final Logger logger = Logger.getLogger(SQLiteConnection.class.getName());
 
-    private static final String DATABASE_PATH = "./database.db";
+    private static final String DEFAULT_PATH = "./database.db";
 
     private Connection connection;
     private Statement statement;
 
-    private SQLiteConnection() {
+    private SQLiteConnection(String dbPath) {
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_PATH);
+            connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
             statement = connection.createStatement();
 
             // Per abilitare il controllo delle chiavi esterne nelle tabelle
@@ -38,15 +46,43 @@ public class SQLiteConnection implements DatabaseConnection {
             SQLitePaidTicket.initTable(statement);
 
         } catch (SQLException e) {
-            logger.severe("Cannot initialize database connection. Please contact software developer.");
+            if (e.getErrorCode() == 8) {
+                logger.severe(e.getMessage());
+            } else {
+                logger.severe("Cannot initialize database connection. Please contact software developer.\n" + e.getErrorCode());
+            }
             System.exit(-1);
         }
-        logger.info(String.format("Database at %s initialized successfully", DATABASE_PATH));
+        logger.info(String.format("Database at %s initialized successfully", dbPath));
     }
 
-    public static synchronized SQLiteConnection getInstance() {
-        if (instance == null) instance = new SQLiteConnection();
-        return instance;
+    private static boolean areFilesEqual(Path p1, Path p2) {
+        try {
+            return Files.isSameFile(p1,p2);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static synchronized SQLiteConnection getInstance(String path) {
+        Path p = Path.of(path);
+        if(!Files.exists(p)) {
+            instances.put(p,new SQLiteConnection(path));
+            return instances.get(p);
+        }
+
+        for(Path pp : instances.keySet()) {
+            if(areFilesEqual(p,pp)) {
+                return instances.get(pp);
+            }
+        }
+
+        instances.put(p,new SQLiteConnection(path));
+        return instances.get(p);
+    }
+
+    public static SQLiteConnection getInstance() {
+        return getInstance(DEFAULT_PATH);
     }
 
     @Override
