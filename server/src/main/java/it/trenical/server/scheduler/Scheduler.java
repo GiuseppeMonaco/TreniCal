e602179b0587f04df51f21e7eafc394774ee93fb
GiuseppeMonaco <61635120+GiuseppeMonaco@users.noreply.github.com>
2025-06-03 03:12:23 +0200

    Fix Scheduler

--- server/src/main/java/it/trenical/server/scheduler/Scheduler.java
+++ server/src/main/java/it/trenical/server/scheduler/Scheduler.java
@@ -1,11 +1,15 @@
 package it.trenical.server.scheduler;
 
+import it.trenical.common.Ticket;
+import it.trenical.common.Trip;
 import it.trenical.server.Server;
 import it.trenical.server.notifications.NotificationManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -36,7 +40,8 @@ public enum Scheduler {
     public void startExpireTask() {
         Runnable task = () -> {
             Calendar now = Calendar.getInstance();
-            server.getTicketsCache().stream().filter(t -> !t.isPaid()).forEach(book -> {
+            Collection<Ticket> tickets = new LinkedList<>(server.getTicketsCache());
+            tickets.stream().filter(t -> !t.isPaid()).forEach(book -> {
 
                 Calendar departure = book.getTrip().getDepartureTime();
                 departure.add(Calendar.HOUR, -HOURS_BEFORE_EXPIRE);
@@ -53,14 +58,15 @@ public enum Scheduler {
                 }
             });
         };
-        scheduler.scheduleAtFixedRate(task, SCHEDULER_RATE, SCHEDULER_START_DELAY, SCHEDULER_RATE_TIMEUNIT);
+        scheduler.scheduleAtFixedRate(task, SCHEDULER_START_DELAY, SCHEDULER_RATE, SCHEDULER_RATE_TIMEUNIT);
         logger.info("Started bookingExpire scheduler");
     }
 
     public void startTripsCleanupTask() {
         Runnable task = () -> {
             Calendar now = Calendar.getInstance();
-            server.getTripsCache().forEach(trip -> {
+            Collection<Trip> trips = new LinkedList<>(server.getTripsCache());
+            trips.forEach(trip -> {
 
                 Calendar departure = trip.getDepartureTime();
                 if (now.after(departure)) {
@@ -69,7 +75,7 @@ public enum Scheduler {
                 }
             });
         };
-        scheduler.scheduleAtFixedRate(task, SCHEDULER_RATE, SCHEDULER_START_DELAY, SCHEDULER_RATE_TIMEUNIT);
+        scheduler.scheduleAtFixedRate(task, SCHEDULER_START_DELAY, SCHEDULER_RATE, SCHEDULER_RATE_TIMEUNIT);
         logger.info("Started cleanup scheduler");
     }
 
